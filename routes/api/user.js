4ffd399536c5a8c7bcc5a72c839b1cb4a4d309a7
mdiktushar <md.tushar1804@gmail.com>
2021-06-02 18:08:20 +0600

    token setup

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../../config/keys')
 
 // Load User model
 const User = require('../../models/Users')
@@ -60,10 +62,26 @@ router.post('/login', (req, res) => {
             return res.status(404).json({email: 'Email not found'})
         }
         bcrypt.compare(password, user.password).then(isMatch => {
-            if(!isMatch) {
+            if(!isMatch) { // Incorrect Passowrd
                 return res.status(400).json({password: 'Incorrect Passowrd '})
             }
-            return res.json({msg: 'Success'})
+            // User Matched 
+            const payload = { // jwt Payload
+                id: user.id,
+                name: user.name,
+                avatar: user.avatar
+            }
+            //Sign Token
+            jwt.sign(
+                payload,
+                keys.secretOrkey,
+                {expiresIn: 86400},
+                (err, token) => {
+                    res.json({
+                        success: true,
+                        token: 'Bearer'+token
+                    })
+                })
         })
 
     })
