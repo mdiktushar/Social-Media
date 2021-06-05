89bb3165b10642aa02a3f70fdaf390635d1af623
mdiktushar <md.tushar1804@gmail.com>
2021-06-05 18:34:01 +0600

    user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const passport = require('passport')
 
+// Loda Profile Model
+const Profile = require('../../models/Profile')
+// Load User Profile
+const User = require('../../models/Users')
 
 // @route  GET api/profile/test
 // @des    Test profile route
@@ -9,6 +15,21 @@ router.get('/test', (req, res) => {
     res.json({
         msg: "Profile Works"
     })
+})
+
+// @route  GET api/profile
+// @des    Get current user prifile
+// @access Private
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const errors ={}
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+        if(!profile){
+            errors.noprofile = 'There is no account for this user'
+            return res.status(404).json(errors)
+        }
+        res.json(profile)
+    }).catch(err => res.status(404).json(err))
 })
 
 module.exports = router;
