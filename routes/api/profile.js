ebf932e7c13f444f59aacf199901e7f1ffe31841
mdiktushar <md.tushar1804@gmail.com>
2021-06-14 01:31:57 +0600

    profile by handle, ID, and All profile (21)

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -37,6 +37,61 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     }).catch(err => res.status(404).json(err))
 })
 
+// @route  GET api/profile/all
+// @des    Get all profile
+// @access public
+
+router.get('/all', (req, res) => {
+    const errors = {}
+
+    Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+        if(!profiles) {
+            errors.noprofile = 'There are no Profiles'
+            return res.status(404).json(errors)
+        }
+        res.json(profiles)
+    }).catch(err => res.status(404).json({profile: 'There are no Profiles'}))
+})
+
+// @route  GET api/profile/handle/:handle
+// @des    Get profile by handle
+// @access public
+
+router.get('/handle/:handle', (req, res) => {
+    const errors = {};
+  
+    Profile.findOne({ handle: req.params.handle })
+      .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+  
+        res.json(profile);
+      }).catch(err => res.status(404).json({profile: 'There is no profile for this user'}))
+  });
+
+// @route  GET api/profile/user/:user_id
+// @des    Get profile by user ID
+// @access public
+
+router.get('/user/:user_id', (req, res) => {
+    const errors = {}
+
+    Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+        if(!profile) {
+            errors.noprofile = 'There is no profile for this user'
+            return res.status(404).json(errors)
+        }
+        res.json(profile)
+    }).catch(err => res.status(404).json({profile: 'There is no profile for this user'}))
+})
+
 // @route  POST api/profile
 // @des    Create or Edit user prifile
 // @access Private
