d65fda6195e068591705d17b1ec7997f5bd8bb23
mdiktushar <md.tushar1804@gmail.com>
2021-06-19 01:48:49 +0600

    delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -271,4 +271,23 @@ router.delete(
         .catch(err => res.status(404).json(err))
     }
 )
+
+// @route  DELETE api/profile
+// @des    Delete user and profile
+// @access private
+router.delete(
+    '/', 
+    passport.authenticate('jwt', { session: false}), 
+    (req, res) => {
+    
+        Profile.findOneAndRemove({ user: req.user.id })
+        .then(()=>{
+            User.findOneAndRemove({ _id: req.user.id })
+            .then(() => {
+                res.json({ success: true})
+            })
+        })
+    }
+)
+
 module.exports = router;
