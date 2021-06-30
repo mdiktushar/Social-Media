70399887b26a368ae8bb9b3aab3ea2eee8218cb3
mdiktushar <md.tushar1804@gmail.com>
2021-07-01 00:24:12 +0600

    see post and delete post (26)

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const { profile_url } = require('gravatar')
 const router = express.Router()
 const mongoose = require('mongoose')
 const passport = require('passport')
 
 // Post modes
 const Post = require('../../models/Post')
+// Profile modes
+const Profile = require('../../models/Profile')
 
 // Validator
 const validatePostInput = require('../../validation/post')
@@ -16,6 +19,25 @@ router.get('/test', (req, res) => {
     res.json({
         msg: "Posts Works"
     })
+})
+
+// @route  GET api/posts
+// @des    GET post public
+// @access Private
+router.get('/', (req, res) => {
+    Post.find()
+    .sort({date: -1})
+    .then( posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostFound: 'No posts found'}))
+})
+
+// @route  GET api/posts/:id
+// @des    GET post by ID
+// @access Private
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+    .then( post => res.json(post))
+    .catch(err => res.status(404).json({ nopostFound: 'No post found with this ID'}))
 })
 
 // @route  POST api/posts
@@ -38,6 +60,26 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     })
 
     newPost.save().then(post => res.json(post))
+})
+
+// @route  DELETE api/posts/:id
+// @des    Delete post
+// @access Private
+router.delete('/:id', passport.authenticate('jwt', { session: false}),(req, res) => {
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
+        Post.findById(req.params.id)
+        .then(post => {
+            // Check for post owner
+            if(post.user.toString() !== req.user.id) {
+                return res.status(401).json({ notauthorized: 'User not authorized' })
+            }
+
+            // Delete
+            post.remove().then(() => res.json({ success: true}))
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found'}))
+    })
 })
 
 module.exports = router;
