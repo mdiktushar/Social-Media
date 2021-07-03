d2ec1f7aa1e4b9425efce6ab9e9f257c0317d308
mdiktushar <md.tushar1804@gmail.com>
2021-07-03 19:45:16 +0600

    error handling display (37)

--- validation/register.js
+++ validation/register.js
@@ -14,7 +14,7 @@ module.exports = function validateRegisterInput(data) {
     }
 
     if(Validator.isEmpty(data.name)) {
-        errors.name = 'Name fild is required'
+        errors.name = 'Name field is required'
     }
 
     if(Validator.isEmpty(data.email)) {
