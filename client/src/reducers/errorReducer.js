3f077d1fbb1f51696ce2524c2484842f1541750e
mdiktushar <md.tushar1804@gmail.com>
2021-07-05 15:58:29 +0600

    registration and error handling (41)

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,13 +1,12 @@
+import { GET_ERRORS } from "../actions/types";
 
-const initialState = {
-    isAuthenticated: false,
-    user: {},
-    hello: 'test'
-};
+const initialState = {};
 
 // to avoid the worning using off defountly it was anonymous function
 export default function foo(state = initialState, action) {
     switch (action.type) {
+        case GET_ERRORS:
+            return action.payload
         default:
             return state;
     }
