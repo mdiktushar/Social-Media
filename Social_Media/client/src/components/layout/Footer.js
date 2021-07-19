95b30b57003ea78c5017f1685f7d39bc98a50841
mdiktushar <md.tushar1804@gmail.com>
2021-07-19 14:25:32 +0600

    project fixed

--- Social_Media/client/src/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <footer className="bg-dark text-white mt-4 p-4 text-center">
-            Copyright &copy; {new Date().getFullYear()} Social Media
-        </footer>
-    )
-}
